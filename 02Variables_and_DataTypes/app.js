792cddf0b54071b2f7b06c8dc18d13342802c1db
ninobegagic <52253431+ninobegagic@users.noreply.github.com>
2019-07-01 14:23:15 +0200

    02 Variables and data types
    Added variables and data types practice

---
+++ 02Variables_and_DataTypes/app.js
@@ -0,0 +1,26 @@
+/*
+//Old way of writing variables in java script
+var life = 100; // this is my life bar NUMBER
+
+life = life - 10;
+
+//life is equal to 90
+
+life = life - 10;
+
+var name = "Dev Ed"; // this is the name displayed in the game STRING
+
+var checkout = false; /BOOLEAN
+
+var box; // doesn't have a value
+
+var box = null; // doesn't have a value
+*/
+
+//const life = 100; //a variable that can't be changed
+
+let life = 100;
+
+life = life - 50;
+
+console.log(life);
